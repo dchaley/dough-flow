417a400665ee80860dec0f895da9a83fde512bb7
David Haley <dchaley@gmail.com>
2017-05-24 01:18:54 -0700

    Adds cash flow interval for once and monthly flows
    Renames cash-flow component parameter `cashflow` to `cashFlow`.
    
    Adds frequency interval printing, e.g., "monthly on the last day"). Works for frequencies: `once`, `monthly`.

--- src/frontend/app/components/cash-flow.js
+++ src/frontend/app/components/cash-flow.js
@@ -1,14 +1,39 @@
 import Ember from 'ember';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export default Ember.Component.extend({
 
   /**
    * True if this cash flow is a withdrawal, i.e., less than zero.
    */
-  isWithdrawal: Ember.computed.lt('cashflow.amount', 0),
+  isWithdrawal: Ember.computed.lt('cashFlow.amount', 0),
 
   /**
    * True if this cash flow is a deposit, i.e., zero or greater.
    */
-  isDeposit: Ember.computed.gte('cashflow.amount', 0),
+  isDeposit: Ember.computed.gte('cashFlow.amount', 0),
+
+  interval: Ember.computed('cashFlow.frequency', function() {
+    var frequency = this.get('cashFlow.frequency');
+    if (frequency == 'once') {
+      return 'on ' + moment(this.get('cashFlow.date')).format('YYYY-MM-DD');
+    } else if (frequency == 'monthly') {
+      var offset = this.get('cashFlow.frequencyOffset');
+      var whichDay;
+      
+      if (offset > 0) {
+        whichDay = numeral(offset).format('Oo');
+      } else {
+        if (offset == -1) {
+          whichDay = 'last';
+        } else {
+          whichDay = numeral(-offset).format('Oo') + ' to last';
+        }
+      }
+
+      return 'on the ' + whichDay + ' day';
+    }
+    return this.get('cashFlow.frequencyOffset');
+  }),
 });
